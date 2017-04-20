1a993e5c7ec8139ebd3b8aca03fb6b0d5632ef99
gonzalofernandez <gonzalo.fernande@hotmail.com>
2017-04-20 17:09:39 +0200

    termino proyecto memoria de elefante

---
+++ examenes_1/memoria_de_elefante/vistas/final.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Final del juego</title>
+    </head>
+    <body>
+        <h1>Memoria de Elefante</h1>
+        <?php echo "<h1>Enhorabuena!!!!!</h1><br><br>"; ?>
+        <form method="post" action="../index.php">
+            Para volver a jugar pulse el botón
+            <input name="nueva_partida" type="submit" value="NUEVA PARTIDA">
+        </form>
+        ?>
+    </body>
+</html>
